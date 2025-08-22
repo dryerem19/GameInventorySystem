e45f1c98712d67cdc626841e83717ba86431729b
Denis Eremenko <mamayma8@gmail.com>
2025-08-22 21:56:50 +0300

    add opengl window and imgui for draw inventory grid

--- main.cpp
+++ main.cpp
@@ -1,9 +1,133 @@
 #include <iostream>
 
-using namespace std;
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
-int main()
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+#define WIDTH   800
+#define HEIGHT  600
+#define TITLE   "Inventory System Demo"
+
+#define INVENTORY_ROWS              5
+#define INVENTORY_COLUMNS           10
+#define INVENTORY_CELL_SIZE         32
+#define INVENTORY_WIDTH             INVENTORY_COLUMNS * INVENTORY_CELL_SIZE
+#define INVENTORY_HEIGHT            INVENTORY_ROWS * INVENTORY_CELL_SIZE
+#define MAX_INVENTORY_WEIGHT        100.0f
+#define INVENTORY_BORDER_COLOR      IM_COL32(200, 200, 200, 255)
+
+float currentWeight = 0;
+
+void draw_inventory(void)
 {
-    cout << "Hello World!" << endl;
+    ImGui::Begin("Inventory", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+    {
+        // Invisible widget so that autoresize works correctly
+        ImGui::InvisibleButton("##inventory_grid",
+                               ImVec2(INVENTORY_WIDTH, INVENTORY_HEIGHT));
+
+        ImDrawList* drawList = ImGui::GetWindowDrawList();
+        ImVec2 cursorPos = ImGui::GetCursorScreenPos();
+        cursorPos.y -= INVENTORY_HEIGHT; // Drawing on top of an invisible widget
+
+        for (int y = 0; y < INVENTORY_ROWS; y++) {
+            for (int x = 0; x < INVENTORY_COLUMNS; x++) {
+                ImVec2 cellMin(cursorPos.x + x * INVENTORY_CELL_SIZE, cursorPos.y + y * INVENTORY_CELL_SIZE);
+                ImVec2 cellMax(cellMin.x + INVENTORY_CELL_SIZE, cellMin.y + INVENTORY_CELL_SIZE);
+                drawList->AddRect(cellMin, cellMax, INVENTORY_BORDER_COLOR);
+            }
+        }
+
+        ImGui::SetCursorPosY(ImGui::GetCursorPosY());
+        ImGui::Text("Weight: %.1f / %.1f", currentWeight, MAX_INVENTORY_WEIGHT);
+    }
+    ImGui::End();
+}
+
+int main(void)
+{
+    if (!glfwInit()) {
+        return -1;
+    }
+
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Ma
+
+    float scale = ImGui_ImplGlfw_GetContentScaleForMonitor(glfwGetPrimaryMonitor());
+    GLFWwindow* window = glfwCreateWindow((int)(WIDTH * scale), (int)(HEIGHT * scale), TITLE, nullptr, nullptr);
+    if (window == nullptr) {
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+        std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // Setup scaling
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.ScaleAllSizes(scale);
+    style.FontScaleDpi = scale;
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0)
+        {
+            ImGui_ImplGlfw_Sleep(10);
+            continue;
+        }
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        draw_inventory();
+
+        // Rendering
+        ImGui::Render();
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
+        glClearColor(0, 0, 0, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return 0;
 }
